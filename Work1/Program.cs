6c9973457f75b641275c82bdc7411d016cb67d83
Sergey <blackwhiterainbow@yandex.ru>
2023-01-14 22:43:15 +0300

    add max aand min number output

---
+++ Work1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int max = a;
+int min = b;
+
+if(max > min)
+{ 
+    max = a; min = b;
+}
+else
+{
+    max = b; min = a;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
