15ede8941fa6d8c1a298b6753e2c7cd463bfb6b9
Sergey <blackwhiterainbow@yandex.ru>
2023-01-14 22:44:06 +0300

    add max number output

---
+++ Work2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая на вход принимает три числа и выдаёт, какое число большее, а какое меньшее
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if(b > max)
+{ 
+    max = b;
+}
+if(c > max)
+{
+   max = c; 
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
