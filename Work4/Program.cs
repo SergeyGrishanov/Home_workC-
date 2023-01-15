450d0559650f9ddf82cf07913a1c364bd5d1f270
Sergey <blackwhiterainbow@yandex.ru>
2023-01-15 23:46:00 +0300

    add even and uneven

---
+++ Work4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пограмму, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if((number % 2) == 0)
+{
+    Console.Write("number = even");
+}
+else
+{
+    Console.Write("number = odd");
+}
